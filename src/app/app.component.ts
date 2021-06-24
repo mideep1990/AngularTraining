2dc74746f786bb877dd804f3131db30d49490aa8
Deepankar Mishra <Deepankar_Mishra@Dell.com>
2021-06-24 20:27:40 +0530

    angular ch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularTraining';
+  someJson = {
+    test: 'some Value',
+    tesr: 'test',
+  };
+
+  test = (f1: any) => {
+    console.log('come Value');
+
+    return '<div>test</div>';
+  };
 }
